b854a98fa9005a1c4479e0404ed96d3f6ec75a1a
kshyamsai <kshyamsai@HSCINHYDLT42593.harsco.com>
2024-08-17 12:41:49 +0530

    First Commit

---
+++ src/Test/Testclass.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test Pass");
+		
+		System.out.println("Pass Test");
+		// TODO Auto-generated method stub
+
+	}
+
+}
